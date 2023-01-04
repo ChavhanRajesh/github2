fd9ee2a64c87351452dda236414e5d9816029032
ChavhanRajesh <chavhanrajesh697@gmail.com>
2023-01-04 17:03:05 +0530

    I want Create java program

---
+++ src/test/java/Test/Class1.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class Class1 {
+	public static void main(String[] args) {
+		
+		System.out.println("Hi am Raj");
+	}
+
+}
---
+++ src/test/java/Test/Class3.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class Class3 {
+	public static void main(String[] args) {
+		System.out.println("I am work in Testyantra");
+	}
+
+}
